555011528385ff2aeac9a092eda5c868970cff02
f4Doll <snowkyuubi@gmail.com>
2020-10-15 18:52:42 -0400

    LoginPanel basic

--- src/View/LoginPanel.java
+++ src/View/LoginPanel.java
@@ -6,14 +6,67 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class LoginPanel extends JPanel
-{
+public class LoginPanel extends JPanel {
+
     private JButton clickMe;
-    
-    public LoginPanel()
-    {
+    private JTextField usern;
+    private JTextField passw;
+
+    public LoginPanel() {
+        //setLayout(new GridLayout(3, 2));
+        usern = new JTextField("Food");
+        passw = new JTextField("Key");
+        JLabel uselbl = new JLabel("Username:");
+        JLabel passlbl = new JLabel("Password:");
         clickMe = new JButton("TESTING: CLICK ME");
+        add(uselbl);
+        add(usern);
+        add(passlbl);
+        add(passw);
         add(clickMe);
+    }
+
+    /**
+     * @return the clickMe
+     */
+    public JButton getClickMe() {
+        return clickMe;
+    }
+
+    /**
+     * @param clickMe the clickMe to set
+     */
+    public void setClickMe(JButton clickMe) {
+        this.clickMe = clickMe;
+    }
+
+    /**
+     * @return the usern
+     */
+    public JTextField getUsern() {
+        return usern;
+    }
+
+    /**
+     * @param usern the usern to set
+     */
+    public void setUsern(JTextField usern) {
+        this.usern = usern;
+    }
+
+    /**
+     * @return the passw
+     */
+    public JTextField getPassw() {
+        return passw;
+    }
+
+    /**
+     * @param passw the passw to set
+     */
+    public void setPassw(JTextField passw) {
+        this.passw = passw;
     }
 }
