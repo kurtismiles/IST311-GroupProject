35b267af9e783b3bc227a1bd6410cd49800ce6c3
Kurtis Miles <kjm6121@psu.edu>
2020-11-02 18:47:48 -0600

    Recipe changes

--- src/Model/RecipeModel.java
+++ src/Model/RecipeModel.java
@@ -9,6 +9,42 @@ package Model;
  *
  * @author Kurtis
  */
-public class RecipeModel {
+public class RecipeModel
+{
+    private RecipeList recipeData;
+    private XML xm;
+    
+    public RecipeModel()
+    {
+        recipeData = new RecipeList();
+    }
+
+    /**
+     * @return the recipeData
+     */
+    public RecipeList getRecipeData()
+    {
+        return recipeData;
+    }
+
+    /**
+     * @param recipeData the recipeData to set
+     */
+    public void setRecipeData(RecipeList recipeData)
+    {
+        this.recipeData = recipeData;
+    }
+    
+    public void writeRecipe(Recipe input)
+    {
+        xm.openWriterXML("recipelist.xml");
+        xm.writeObject(input);
+        xm.closeWriterXML();
+    }
+    
+    public void refreshRecipeList()
+    {
+        recipeData = new RecipeList();
+    }
     
 }
